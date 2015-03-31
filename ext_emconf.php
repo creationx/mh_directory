0d3ca29ef8e70e2d330487774206e82bbd5d4206
Martin Hesse <mhesse85@gmail.com>
2015-03-31 20:46:21 +0200

    ext_emconf update

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,26 +6,21 @@ $EM_CONF[$_EXTKEY] = array (
 	'category' => 'plugin',
 	'version' => '0.5.0',
 	'state' => 'beta',
-	'uploadfolder' => 0,
-	'createDirs' => '',
+	'uploadfolder' => 1,
+	'createDirs' => 'uploads/tx_mhdirectory',
 	'clearcacheonload' => 0,
 	'author' => 'Martin Hesse',
 	'author_email' => 'info@mh-dev.de',
 	'author_company' => 'MH-Dev. - Webentwicklung',
 	'constraints' => 
 	array (
-		'depends' => 
-		array (
+		'depends' => array (
 			'php' => '5.3.7-0.0.0',
-			'typo3' => '6.0.0-6.2.99',
-			'vhs' => '2.3.1-0.0.0'
+			'typo3' => '6.0.0-7.1.99',
+			'vhs' => '2.3.0-0.0.0'
 		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
+		'conflicts' => array (),
+		'suggests' => array (),
 	),
 );
 
