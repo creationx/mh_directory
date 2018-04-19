ba7d22d634ca49f6b6a66e9f9e7b313164aa34ab
Stefan Kress <sphkress@gmail.com>
2018-04-19 17:46:16 +0200

    Compatibility changes for TYPO3 v8.7

--- Resources/Public/Js/frontend.js
+++ Resources/Public/Js/frontend.js
@@ -1,26 +1,26 @@
-jQuery(function() {
+jQuery(function () {
 
-	var sHash 			= window.location.hash;
-	var oAlphaMenu 		= jQuery('div.tx_mhdirectory > ul.alphabetical_menu');
-	var bStateAllLetter	= false;
+	var sHash = window.location.hash;
+	var oAlphaMenu = jQuery('div.tx_mhdirectory > ul.alphabetical_menu');
+	var bStateAllLetter = false;
 
 
-	if(sHash.length > 0 && oAlphaMenu.length > 0) {
+	if (sHash.length > 0 && oAlphaMenu.length > 0) {
 		openLetterEntry(sHash.substring(1));
 	}
 
-	if(jQuery('ul.alphabetical_menu').length > 0) {
+	if (jQuery('ul.alphabetical_menu').length > 0) {
 		var oTarget = jQuery('ul.alphabetical_menu > li.active');
-		if(oTarget.length == 1)
+		if (oTarget.length == 1)
 			openLetterEntry(oTarget.html());
 	}
 
-	jQuery('body').on('click', 'div.tx_mhdirectory > ul.alphabetical_menu > li', function(e) {
+	jQuery('body').on('click', 'div.tx_mhdirectory > ul.alphabetical_menu > li', function (e) {
 		var sChar = jQuery(this).html();
 		openLetterEntry(sChar);
 	});
 
-	jQuery('body').on('click', 'div.tx_mhdirectory > ul.alphabetical_menu > li.abc_all', function(e) {
+	jQuery('body').on('click', 'div.tx_mhdirectory > ul.alphabetical_menu > li.abc_all', function (e) {
 		openAllLetter();
 	});
 
@@ -28,15 +28,15 @@ jQuery(function() {
 		var oTarget = jQuery('div.letter_' + key + ' > div');
 		var oLi = jQuery('li[rel="letter_li_' + key + '"]');
 
-		if(oTarget.length > 0) {
+		if (oTarget.length > 0) {
 			// fix the "hidden div" bug for gmap
-			oTarget.each(function(i, v) {
+			oTarget.each(function (i, v) {
 				var sMapId = jQuery('div.mh_directory_gmap', v).attr('id');
-				if(typeof sMapId != 'undefined') {
-					setTimeout(function() {
+				if (typeof sMapId != 'undefined') {
+					setTimeout(function () {
 						var oTmpMapDom = jQuery('#' + sMapId)[0];
 						google.maps.event.trigger(oTmpMapDom, 'resize');
-						var aTmpMapId	= sMapId.split('_')
+						var aTmpMapId = sMapId.split('_')
 						var oTmpMap = 'map_' + aTmpMapId[1];
 						eval(oTmpMap).setCenter(aMapPos[aTmpMapId[1]]);
 					}, 1000);
@@ -53,7 +53,7 @@ jQuery(function() {
 	}
 
 	function openAllLetter() {
-		if(!bStateAllLetter) {
+		if (!bStateAllLetter) {
 			jQuery('div.letter_entries').show();
 			jQuery('div.letter_choose').hide();
 			bStateAllLetter = true;
@@ -64,4 +64,3 @@ jQuery(function() {
 		}
 	}
 });
-
